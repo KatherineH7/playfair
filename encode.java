73a286a00686b41016ce240a2a1d2dace0c73713
KatherineH7 <khlaing10@stuy.edu>
2020-10-11 19:50:38 -0400

    split into two, got one X to replace double

--- encode.java
+++ encode.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class encode
 {
   public static void main(String[] args)
   {
     File inFile = null;
+    String message = "";
     if (0 < args.length)
     {
       inFile = new File(args[0]);
@@ -28,6 +30,7 @@ public class encode
 
         while ((sCurrentLine = br.readLine()) != null)
           {
+            message += sCurrentLine;
             System.out.println(sCurrentLine);
           }
 
@@ -49,5 +52,39 @@ public class encode
             ex.printStackTrace();
           }
         }
+
+
+
+        message = message.toUpperCase();
+        message = message.replaceAll("\\p{Punct}", "");
+        message = message.replaceAll("\\d","");
+        System.out.println(message);
+
+        ArrayList<Character> ch = new ArrayList<Character>();
+        for (int i = 0; i < message.length(); i++)
+        {
+            ch.add(message.charAt(i));
+        }
+
+        for (int x = 0; ch.get(x).equals(ch.get(x+1)) && x % 2 == 0; x++)
+          {
+            int y = ch.size() - 1;
+            ch.add(ch.get(y));
+            while (y > x + 2)
+            {
+              ch.set(y,ch.get(y-1));
+              y--;
+              ch.set(x+1, 'X');
+            }
+          }
+
+        for (int z = 0; z < ch.size(); z++)
+        {
+            if (z % 2 == 0)
+              System.out.print(ch.get(z));
+            else
+              System.out.print(ch.get(z) + " ");
+        }
+
   }
 }
